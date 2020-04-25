d4c098f23df7f918f401f754868cae57736770cc
Ali Rahemi <deutschlandmyhome@gmail.com>
2020-04-26 00:31:18 +0200

    finish the authentication with my own login page

--- src/main/java/ir/arcademy/blog/BlogApplication.java
+++ src/main/java/ir/arcademy/blog/BlogApplication.java
@@ -2,11 +2,14 @@ package ir.arcademy.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
+        //System.out.println(new BCryptPasswordEncoder().encode("1"));
+
         SpringApplication.run(BlogApplication.class, args);
     }
 
--- src/main/java/ir/arcademy/blog/config/SpringSecurityConfig.java
+++ src/main/java/ir/arcademy/blog/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -18,18 +19,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(new BCryptPasswordEncoder())
                 .usersByUsernameQuery("select email,password,enabled from users_tbl where email=?")
                 .authoritiesByUsernameQuery("select email,roles from authorities where email=?");
     }
 
-   /* @Override
+   @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/","index","/css/**","/js/**","/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin()
-                .loginPage("/login").permitAll()
+                .loginPage("/login").usernameParameter("email").permitAll()
                 .and().logout().permitAll();
-    }*/
+    }
 }
